46b598b38a9ecf2989175932d2724e350cc967d1
tushar8871 <tusharkhachane6@gmail.com>
2020-03-20 14:08:43 +0530

    Added : Check either gambler win or loose

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -12,3 +12,13 @@ bet=0;
 read -p "Enter stack amount : " stackWallet
 read -p "Enter goal amount : " GOAL
 read -p "Enter bet : " bet
+
+gamblerTurn=$((RANDOM%2))
+
+#0 for win and 1 for loose 
+if [ $gamblerTurn -eq 0 ]
+then
+	stackWallet=$((stackWallet+bet))
+else
+	stackWallet=$((stackWallet-bet))
+fi
