a707445bcdac54edb188ff844e85c2dcab178430
tushar8871 <tusharkhachane6@gmail.com>
2020-03-20 18:05:02 +0530

    Added : determine luckiest day or unluckiest day in the month

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -11,9 +11,46 @@ stackWallet=0;
 bet=0;
 amountWon=0;
 amountLoss=0;
-totalDays=0;
-daysWon=0;
-daysLoss=0;
+totalDays=1;
+maxAmount=0;
+minAmount=0;
+wonDay=1;
+lossDay=1;
+day=1;
+wonday=0;
+lossday=0;
+
+#declaring array
+
+declare -A wonDays
+declare -A lossDays
+
+#max function won and loss
+function maxWonDay(){
+	local largest=$1;
+	for (( day=1;$day<$2; day++ ))
+	do
+		if [[ ${wonDays[$day]} -gt $largest ]]
+		then
+			largest=${wonDays[$day]};
+			wonday=$day;
+		fi
+	done
+	echo $wonday
+}
+
+function maxLossDay(){
+	local largest=$1;
+	for (( day=1;$day<$2; day++ ))
+	do
+		if [[ ${lossDays[$day]} -gt $largest ]]
+		then
+			largest=${lossDays[$day]};
+			lossday=$day;
+		fi
+	done
+	echo $lossday
+}
 
 # read value from user
 read -p "Enter stack amount : " stackWallet
@@ -21,23 +58,34 @@ read -p "Enter goal amount : " GOAL
 read -p "Enter bet : " bet
 
 CHECKWALLETAMOUNT=$(($stackWallet/2))
-
-while [[ $totalDays -lt 20 && $stackWallet -ge $CHECKWALLETAMOUNT && $stackWalletAmount -le $(($CHECHWALLETAMOUNT+$stackWallet)) ]]
+while [[ $totalDays -le 20 ]]
 do
-	gamblerTurn=$((RANDOM%2))
-	#0 for win and 1 for loose 
+	amountWon=0;
+	amountLoss=0;
+	stackWallet=100;
+	while [[ $stackWallet -gt $CHECKWALLETAMOUNT && $stackWallet -lt $((50+100)) ]]
+	do
+		gamblerTurn=$((RANDOM%2))
+		#0 for win and 1 for loose 
 
-	if [ $gamblerTurn -eq 0 ]
-	then
-		stackWallet=$((stackWallet+bet))
-		amountWon=$((amountWon+bet))
-		((daysWon++))
-	else
-		stackWallet=$((stackWallet-bet))
-		amountLoss=$((amountLoss+bet))
-		((daysLoss++))
-	fi
+		if [ $gamblerTurn -eq 0 ]
+		then
+			stackWallet=$((stackWallet+bet))
+			amountWon=$((amountWon+bet))
+		else
+			stackWallet=$((stackWallet-bet))
+			amountLoss=$((amountLoss+bet))
+		fi
+	done
+	wonDays[$wonDay]=$amountWon
+	((wonDay++))
+	lossDays[$lossDay]=$amountLoss
+	((lossDay++))
 	((totalDays++))
 done
-echo "total days won : $daysWon"
-echo "total days loss : $daysLoss"
+
+luckDay=$( maxWonDay ${wonDays[1]} $wonDay )
+unluckDay=$( maxLossDay ${lossDays[1]} $lossDay )
+
+echo "Luckiest day : $luckDay"
+echo "Unluckiest day : $unluckDay"
