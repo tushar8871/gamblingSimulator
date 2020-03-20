66b4d65376df3bba0a7e9af1afb6f5a18d2224a5
tushar8871 <tusharkhachane6@gmail.com>
2020-03-20 13:59:19 +0530

    Added : Read stack,goal and bet amount from user

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Weolcome to Gambler Simulator"
+
+#constants
+GOAL=0;
+
+#variables
+stackWallet=0;
+bet=0;
+
+read -p "Enter stack amount : " stackWallet
+read -p "Enter goal amount : " GOAL
+read -p "Enter bet : " bet
