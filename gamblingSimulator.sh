6e3133a2edcf98bda20a4260150afb427e84b64c
tushar8871 <tusharkhachane6@gmail.com>
2020-03-20 15:19:17 +0530

    Added : find the total amount loose or won

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -9,14 +9,18 @@ CHECKWALLETAMOUNT=0;
 #variables
 stackWallet=0;
 bet=0;
+amountWon=0;
+amountLoss=0;
+days=0;
 
+# read value from user
 read -p "Enter stack amount : " stackWallet
 read -p "Enter goal amount : " GOAL
 read -p "Enter bet : " bet
 
 CHECKWALLETAMOUNT=$(($stackWallet/2))
 
-while [[ $stackWallet -ge $CHECKWALLETAMOUNT && $stackWalletAmount -le $(($CHECHWALLETAMOUNT+$stackWallet)) ]]
+while [[ $days -le 20 && $stackWallet -ge $CHECKWALLETAMOUNT && $stackWalletAmount -le $(($CHECHWALLETAMOUNT+$stackWallet)) ]]
 do
 	gamblerTurn=$((RANDOM%2))
 	#0 for win and 1 for loose 
@@ -24,7 +28,9 @@ do
 	if [ $gamblerTurn -eq 0 ]
 	then
 		stackWallet=$((stackWallet+bet))
+		amountWon=$((amountWon+bet))
 	else
 		stackWallet=$((stackWallet-bet))
+		amountLoss=$((amountLoss+bet))
 	fi
 done
